83d92ba676e59e6d6be26189be99c768cef04eba
Juan David Salamanca <salamancajd14@gmail.com>
2024-04-02 20:47:47 -0500

    Add completion functionalities

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,65 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "bashls",
-          "cssls",
-          "dockerls",
-          "docker_compose_language_service",
-          "eslint",
-          "html",
-          --"htmx",
-          "jsonls",
-          "tsserver",
-          "jinja_lsp",
-          "markdown_oxide",
-          "prismals",
-          "pyright",
-          "rust_analyzer",
-          "sqlls",
-          "slint_lsp",
-          "somesass_ls",
-          "taplo",
-          "tailwindcss"
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.pyright.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.lua_ls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"bashls",
+					"cssls",
+					"dockerls",
+					"docker_compose_language_service",
+					"eslint",
+					"html",
+					--"htmx",
+					"jsonls",
+					"tsserver",
+					"jinja_lsp",
+					"markdown_oxide",
+					"prismals",
+					"pyright",
+					"rust_analyzer",
+					"sqlls",
+					"slint_lsp",
+					"somesass_ls",
+					"taplo",
+					"tailwindcss",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier",
+					"stylua",
+					"isort",
+					"black",
+					"pylint",
+					"eslint_d",
+				},
+			})
+		end,
+	},
 }
-
