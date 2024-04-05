470c760da9d447b5f333362db650f27408ece717
Juan David Salamanca <salamancajd14@gmail.com>
2024-04-05 10:20:00 -0500

    Add tmux integration

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,4 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
-
+vim.keymap.set("n", "<c-k>", ":windcmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":windcmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":windcmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":windcmd l<CR>")
