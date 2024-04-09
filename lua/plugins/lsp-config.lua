b3afa10db52d2daecc89311cd6fb99a92be9aaf6
Juan David Salamanca <salamancajd14@gmail.com>
2024-04-09 18:39:52 -0500

    Add autoclose tags and autopairs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -42,6 +42,7 @@ return {
 			lspconfig.pyright.setup({ capabilities = capabilities })
 			lspconfig.tsserver.setup({ capabilities = capabilities })
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.prismals.setup({ capabilities = capabilities })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
