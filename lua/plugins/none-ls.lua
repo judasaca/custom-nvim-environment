a90bcec728c0a9048116e81a77727d92c927b045
Juan David Salamanca <salamancajd14@gmail.com>
2024-04-24 12:34:46 -0500

    add colorizer plugin for strings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
 		local null_ls = require("null-ls")
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
-			debug = true,
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				require("none-ls.diagnostics.eslint_d"),
