4de0783d0546f4ae3c5ea66046650dba173386f0
Juan David Salamanca <salamancajd14@gmail.com>
2025-02-02 21:34:52 +0100

    Update local configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,11 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons'},
-  config = function() 
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    }
-    )
-  end
+	"nvim-lualine/lualine.nvim",
+	requires = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+		})
+	end,
 }
